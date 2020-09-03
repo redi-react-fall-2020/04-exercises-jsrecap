3da01133f3e098853b58a31a880d3c7c429cc0ab
emrekaraca <creagas@googlemail.com>
2020-09-03 18:26:05 +0200

    Added exercise code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,111 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import team from "./team";
+import {
+  getStudents,
+  getTeachers,
+  getMemberAnimalsWithUrls,
+  getAnimals,
+  filterByAnimal,
+} from "./student.js";
 
 function App() {
+  const [showRole, setShowRole] = useState("all");
+  const [showAnimal, setShowAnimal] = useState("all");
+  const [members, setMembers] = useState(team);
+
+  useEffect(() => {
+    let filteredMembers = [];
+    if (showRole === "teachers") {
+      filteredMembers = getTeachers(team);
+    } else if (showRole === "students") {
+      filteredMembers = getStudents(team);
+    } else {
+      filteredMembers = team;
+    }
+    if (showAnimal !== "all") {
+      filteredMembers = filterByAnimal(filteredMembers, showAnimal);
+    }
+    setMembers(filteredMembers);
+  }, [showAnimal, showRole]);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Redi React Fall 2020 Team</h1>
       </header>
+      <main>
+        <div className="filters-area">
+          <label>
+            <span>Show Role:</span>
+            <select
+              id="select-role"
+              value={showRole}
+              onChange={(event) => setShowRole(event.target.value)}
+            >
+              <option value="all">All Members</option>
+              <option value="students">Students</option>
+              <option value="teachers">Teachers</option>
+            </select>
+          </label>
+          <label>
+            <span>Filter by animals:</span>
+            <select
+              id="select-animal"
+              value={showAnimal}
+              onChange={(event) => setShowAnimal(event.target.value)}
+            >
+              <option value="all">All Animals</option>
+              {getAnimals(team).map((animal) => {
+                return (
+                  <option
+                    value={animal}
+                    key={`${animal}-option`}
+                  >{`${animal.slice(0, 1).toUpperCase()}${animal.slice(
+                    1
+                  )}`}</option>
+                );
+              })}
+            </select>
+          </label>
+        </div>
+        <table className="members-table">
+          <thead>
+            <tr>
+              <th>Photo</th>
+              <th>Name</th>
+              <th>Role</th>
+              <th>Animals</th>
+            </tr>
+          </thead>
+          <tbody>
+            {members.map((member) => (
+              <tr className="member-row" key={member.name}>
+                <td className="avatar-cell">
+                  <img src={member.image} />
+                </td>
+                <td className="name-cell">{member.name}</td>
+                <td className="role-cell">{member.role}</td>
+                <td className="animals-cell">
+                  {getMemberAnimalsWithUrls(member).map((animal) => (
+                    <div>
+                      <img
+                        src={animal.url}
+                        alt={animal.name}
+                        title={animal.name}
+                        key={`${animal.name}-${member.name}`}
+                      />
+                      <span>{animal.name}</span>
+                    </div>
+                  ))}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </main>
     </div>
   );
 }
---
+++ src/student.js
@@ -0,0 +1,25 @@
+const getStudents = (team) => {
+  return team;
+};
+const getTeachers = (team) => {
+  return team;
+};
+
+const getMemberAnimalsWithUrls = (member) => {
+  return [];
+};
+const getAnimals = (team) => {
+  return [];
+};
+
+const filterByAnimal = (team, animal) => {
+  return team;
+};
+
+export {
+  getStudents,
+  getTeachers,
+  getMemberAnimalsWithUrls,
+  getAnimals,
+  filterByAnimal,
+};
